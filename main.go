d11a0f55353b5a3df959b69d40ba4f447dc06e3a
halka asahina <halka.rjch@gmail.com>
2021-08-06 20:05:27 +0900

    remove comment

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/109/:startId/:poleId", func(c *gin.Context) {
+	r.GET("/approaching/:startId/:poleId", func(c *gin.Context) {
 		params := parser.SetParams(c.Params.ByName("startId"), c.Params.ByName("poleId"))
 		c.JSON(http.StatusOK, parser.ParseApproaching(params))
 	})
@@ -17,6 +17,5 @@ func setupRouter() *gin.Engine {
 }
 func main() {
 	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
 	r.Run(":3001")
 }
